2c3ba775cede52029f095836206bb0001025be18
Leonwisch <leon.faessler@yahoo.com>
2024-05-17 13:06:21 +0200

    Aufgabe1.8

--- Aufgabe-1.7/Script.js
+++ Aufgabe-1.7/Script.js
@@ -1,3 +1,4 @@
+function Hallo(){
 let name = prompt("Please enter your name:");
 let d = new Date();
 let hour = d.getHours();
@@ -7,4 +8,19 @@ if (hour < 12) {
     alert("Good Day, " + name + "!");
 }else{
     alert("Good Night, " + name + "!");
+}
+}
+
+function guessNumber() {
+    let randomNumber = Math.floor(Math.random() * 100);
+    let guessedNumber;
+    do {
+        guessedNumber = prompt("Guess a Number between 0 and 100: ");
+        if (guessedNumber > randomNumber) {
+            alert("Wrong Number. Random number was lower");
+        } else if (guessedNumber < randomNumber) {
+            alert("Wrong Number. Random number was higher");
+        }
+    } while (guessedNumber != randomNumber);
+    alert("Guessed Correct");
 }
