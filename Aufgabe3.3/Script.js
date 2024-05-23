04a46ba9731c547bcc794c2e01b6fb79dcea3787
Leonwisch <leon.faessler@yahoo.com>
2024-05-23 15:32:58 +0200

    Aufgabe3.3

---
+++ Aufgabe3.3/Script.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+    if (!event.target.closest('.dropbtn')) {
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
